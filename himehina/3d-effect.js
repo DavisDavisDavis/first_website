71841c7adbdc9f46efaed4cdda75cf3fb05ee3a8
DavisDavisDavis <olizoooom@gmail.com>
2021-10-24 14:17:46 +0200

    3d-effect-with java
    + cool 3d
    + extra animation on first load

---
+++ himehina/3d-effect.js
@@ -0,0 +1,32 @@
+//Items
+const card = document.querySelector('body main');
+const container = document.querySelector('body main');
+
+const release = document.querySelector('main .release');
+
+//Animation event
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10; 
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10; 
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    
+});
+
+//Animate In
+card.addEventListener('mouseenter', (e) => {
+    card.style.transition = 'none';
+
+    //Cool stuff
+    release.style.transform = 'translateZ(200px) perspective(200px)';
+
+});
+
+//Animate out
+card.addEventListener('mouseleave', (e) => {
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+    release.style.transform = 'translateZ(0px)';
+
+    release.style.background = 'none';
+})
--- himehina/index.php
+++ himehina/index.php
@@ -10,8 +10,9 @@ require __DIR__ ."/function.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <title>HIMEHINA</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" type="image/jpg" href="https://stickershop.line-scdn.net/stickershop/v1/sticker/99009942/android/sticker.png"/>
 </head>
 <body>
     <div class="marquee">
@@ -40,7 +41,7 @@ require __DIR__ ."/function.php";
         </div>
     </div>
     <main>
-        <img src="/images/Tanakahime-removebg-preview.png" alt="">
+        <img class="hime_img" src="/images/Tanakahime-removebg-preview.png" alt="">
 
         <a href="album.php">
             <div class="release">
@@ -61,7 +62,7 @@ require __DIR__ ."/function.php";
 
 
         
-        <img src="/images/SuzukiHina-removebg-preview.png" alt="">
+        <img class="hina_img" src="/images/SuzukiHina-removebg-preview.png" alt="">
     </main>
     <div class="marquee">
         <div class="marquee_content_bot">
@@ -89,3 +90,5 @@ require __DIR__ ."/function.php";
     </div>
 </body>
 </html>
+
+<script type="text/javascript" src="3d-effect.js"></script>
